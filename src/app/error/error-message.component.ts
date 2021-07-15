b183a06530cf207ac3e6fff1786a755f1b71ded7
Samuel Tijani <omalitijani@gmail.com>
2021-07-15 17:21:28 +0100

    Increment no of attempts

--- src/app/error/error-message.component.ts
+++ src/app/error/error-message.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ErrorMessageComponent implements OnInit {
 
   @Input() message;
-  @Input() numberOfAttempts;
+  @Input() numberOfAttempts = 12;
   time: any;
 
   constructor() {
